ef813ee4b972ac124c26ee186953d7b4b60398a2
“Alan <alanlee040702@gmail.com>
2023-07-25 22:22:43 -0400

    make panzoom possible when drawing

--- a4/app/src/main/java/com/example/pdfreader/util/CustomScrollView.kt
+++ a4/app/src/main/java/com/example/pdfreader/util/CustomScrollView.kt
@@ -40,106 +40,112 @@ class CustomScrollView @JvmOverloads constructor(
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (isScrollEnabled) {
-            when (event.pointerCount) {
-                1 -> return super.onTouchEvent(event)
-                2 -> {
-                    var inverted = floatArrayOf()
-                    p1_id = event.getPointerId(0)
-                    p1_index = event.findPointerIndex(p1_id)
-
-                    inverted = floatArrayOf(event.getX(p1_index), event.getY(p1_index))
-                    inverse.mapPoints(inverted)
-
-                    if (old_x1 < 0 || old_y1 < 0) {
-                        x1 = inverted.get(0)
-                        old_x1 = x1
-                        y1 = inverted.get(1)
-                        old_y1 = y1
-                    } else {
-                        old_x1 = x1
-                        old_y1 = y1
-                        x1 = inverted.get(0)
-                        y1 = inverted.get(1)
-                    }
-
-                    p2_id = event.getPointerId(1)
-                    p2_index = event.findPointerIndex(p2_id)
-
-                    inverted = floatArrayOf(event.getX(p2_index), event.getY(p2_index))
-                    inverse.mapPoints(inverted)
-
-                    if (old_x2 < 0 || old_y2 < 0) {
-                        x2 = inverted.get(0)
-                        old_x2 = x2
-                        y2 = inverted.get(1)
-                        old_y2 = y2
-                    } else {
-                        old_x2 = x2
-                        old_y2 = y2
-                        x2 = inverted.get(0)
-                        y2 = inverted.get(1)
-                    }
-
-                    mid_x = (x1 + x2) / 2
-                    mid_y = (y1 + y2) / 2
-                    old_mid_x = (old_x1 + old_x2) / 2
-                    old_mid_y = (old_y1 + old_y2) / 2
-
-                    val d_old =
-                        Math.sqrt(Math.pow((old_x1 - old_x2).toDouble(), 2.0) + Math.pow((old_y1 - old_y2).toDouble(), 2.0))
-                            .toFloat()
-                    val d = Math.sqrt(Math.pow((x1 - x2).toDouble(), 2.0) + Math.pow((y1 - y2).toDouble(), 2.0))
-                        .toFloat()
-
-                    if (event.action == MotionEvent.ACTION_MOVE) {
-                        val dx = mid_x - old_mid_x
-                        val dy = mid_y - old_mid_y
-                        currentMatrix.preTranslate(dx, dy)
-
-                        var scale = d / d_old
-                        scale = Math.max(0f, scale)
-                        currentMatrix.preScale(scale, scale, mid_x, mid_y)
-                    } else if (event.action == MotionEvent.ACTION_UP) {
-                        old_x1 = -1f
-                        old_y1 = -1f
-                        old_x2 = -1f
-                        old_y2 = -1f
-                        old_mid_x = -1f
-                        old_mid_y = -1f
-                    }
-                    pdfViewModel?.setTransform(currentMatrix)
-                    return true
-                }
-                else -> return false
+            return when(event.pointerCount) {
+                2 -> panZoom(event)
+                else -> super.onTouchEvent(event)
             }
         } else {
-            val matrix = FloatArray(9)
-            currentMatrix.getValues(matrix)
+            when(event.pointerCount) {
+                1 -> {
+                    val matrix = FloatArray(9)
+                    currentMatrix.getValues(matrix)
 
-            val tx = matrix[Matrix.MTRANS_X]
-            val ty = matrix[Matrix.MTRANS_Y]
-            val sx = matrix[Matrix.MSCALE_X]
-            val sy = matrix[Matrix.MSCALE_Y]
+                    val tx = matrix[Matrix.MTRANS_X]
+                    val ty = matrix[Matrix.MTRANS_Y]
+                    val sx = matrix[Matrix.MSCALE_X]
+                    val sy = matrix[Matrix.MSCALE_Y]
 
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    path = Path()
-                    path?.moveTo(scrollX + (event.x - tx) / sx, scrollY + (event.y - ty) / sy)
-                    pdfViewModel?.setPath(path!!)
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            path = Path()
+                            path?.moveTo((scrollX + event.x - tx) / sx,(scrollY + event.y - ty) / sy)
+                            pdfViewModel?.setPath(path!!)
+                        }
+
+                        MotionEvent.ACTION_MOVE -> {
+                            path?.lineTo((scrollX + event.x - tx) / sx,(scrollY + event.y - ty) / sy)
+                            pdfViewModel?.setPath(path!!)
+                        }
+
+                        MotionEvent.ACTION_UP -> {
+                            pdfViewModel?.setPath(null)
+                            pdfViewModel?.addPath(path)
+                            path = null
+                        }
+                    }
                 }
-
-                MotionEvent.ACTION_MOVE -> {
-                    path?.lineTo(scrollX + (event.x - tx) / sx, scrollY + (event.y - ty) / sy)
-                    pdfViewModel?.setPath(path!!)
-                }
-
-                MotionEvent.ACTION_UP -> {
-                    pdfViewModel?.setPath(null)
-                    pdfViewModel?.addPath(path)
-                    path = null
-                }
+                2 -> panZoom(event)
             }
             return true
         }
+    }
+    private fun panZoom(event: MotionEvent) : Boolean {
+        path = null
+        var inverted = floatArrayOf()
+        p1_id = event.getPointerId(0)
+        p1_index = event.findPointerIndex(p1_id)
+
+        inverted = floatArrayOf(event.getX(p1_index), event.getY(p1_index))
+        inverse.mapPoints(inverted)
+
+        if (old_x1 < 0 || old_y1 < 0) {
+            x1 = inverted.get(0)
+            old_x1 = x1
+            y1 = inverted.get(1)
+            old_y1 = y1
+        } else {
+            old_x1 = x1
+            old_y1 = y1
+            x1 = inverted.get(0)
+            y1 = inverted.get(1)
+        }
+
+        p2_id = event.getPointerId(1)
+        p2_index = event.findPointerIndex(p2_id)
+
+        inverted = floatArrayOf(event.getX(p2_index), event.getY(p2_index))
+        inverse.mapPoints(inverted)
+
+        if (old_x2 < 0 || old_y2 < 0) {
+            x2 = inverted.get(0)
+            old_x2 = x2
+            y2 = inverted.get(1)
+            old_y2 = y2
+        } else {
+            old_x2 = x2
+            old_y2 = y2
+            x2 = inverted.get(0)
+            y2 = inverted.get(1)
+        }
+
+        mid_x = (x1 + x2) / 2
+        mid_y = (y1 + y2) / 2
+        old_mid_x = (old_x1 + old_x2) / 2
+        old_mid_y = (old_y1 + old_y2) / 2
+
+        val d_old =
+            Math.sqrt(Math.pow((old_x1 - old_x2).toDouble(), 2.0) + Math.pow((old_y1 - old_y2).toDouble(), 2.0))
+                .toFloat()
+        val d = Math.sqrt(Math.pow((x1 - x2).toDouble(), 2.0) + Math.pow((y1 - y2).toDouble(), 2.0))
+            .toFloat()
+
+        if (event.action == MotionEvent.ACTION_MOVE) {
+            val dx = mid_x - old_mid_x
+            val dy = mid_y - old_mid_y
+            currentMatrix.preTranslate(dx, dy)
+
+            var scale = d / d_old
+            scale = Math.max(0f, scale)
+            currentMatrix.preScale(scale, scale, mid_x, mid_y)
+        } else if (event.action == MotionEvent.ACTION_UP) {
+            old_x1 = -1f
+            old_y1 = -1f
+            old_x2 = -1f
+            old_y2 = -1f
+            old_mid_x = -1f
+            old_mid_y = -1f
+        }
+        pdfViewModel?.setTransform(currentMatrix)
+        return true
     }
 }
